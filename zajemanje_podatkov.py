869e4a5c5a8d5b713e096267e2e4d35d3f021e6e
Katja Kranjec <Kranjec.katja1@gmail.com>
2020-11-08 22:39:35 +0100

    vzorec filma in pobiranje posameznih strani

--- zajemanje_podatkov.py
+++ zajemanje_podatkov.py
@@ -5,9 +5,25 @@ import json
 
 vzorec_povezave = (r'<a href="(?P<povezava>.*?)" class="title"><h3>(?P<naslov>.*?)</h3></a>')
 
+vzorec_filma = re.compile(
+    r'<div class="product_title">.*?<h1>(?P<naslov>.+?)</h1>.*?'
+    r'<span class="platform">\s*(<a href=.*?>)?\s*(?P<platforma>.+?)\s{2}.*?</span>.*?'
+    r'<span class="label">Publisher:</span>\s*?<span class="data">\s*(<a href=.*?>)?\s*(?P<podjetje>.+?)\s{2}.*?'
+    r'<span class="label">Release Date:</span>\s*?<span class="data" >(?P<datum>.+?)</span>.*?'
+    r'<div class="label">Metascore</div>.*?<span>(?P<metascore>\d{2})</span>.*?'
+    r'<span class="based">based on</span>\s*?.*?(?P<stevilo_glasov_metascore>\d+)\s*?</span> Critic Reviews.*?'
+    r'<div class="label">User Score</div>.*?(?P<ocena_uporabnikov>\d\.\d)</div>.*?'
+    r'<span class="based">based on</span>\s*?.*?(?P<stevilo_glasov_uporabnikov>\d+)\sRatings.*?'
+    r'<span class="label">Summary:</span>\s*?<span class="data" >(?P<opis>.+?)</span>.*?',
+    flags=re.DOTALL
+)
+
 najdene_igre = 0
 igre = []
-STEVILO_STRANI = 6
+video_igre = 0
+STEVILO_STRANI = 1
+najdene_video_igre = 0
+
 
 for stran in range(STEVILO_STRANI):
     if stran == 0:
@@ -26,6 +42,20 @@ for stran in range(STEVILO_STRANI):
 
 datoteka_s_slovarjem = 'povezave-do-iger.json'
 with open(datoteka_s_slovarjem, 'w', encoding='utf-8') as f:
-            json.dump(igre, f)
+    json.dump(igre, f)
 
 print(najdene_igre)
+
+with open(datoteka_s_slovarjem, 'r', encoding='utf-8') as f:
+    igre = json.load(f)
+    for igra in igre:
+        video_igre +=1
+        povezava = igra.get("povezava")
+        url = f'https://www.metacritic.com{povezava}/details'
+        datoteka = f'najbolj-znane-video-igre/video-igra{video_igre}.html'
+        orodja.shrani_spletno_stran(url, datoteka)
+        vsebina = orodja.vsebina_datoteke(datoteka)
+
+        for zadetek in re.finditer(vzorec_filma, vsebina):
+            print(zadetek.groupdict())
+            najdene_video_igre += 1
