fb4c52d677c383fabd95280d3289ea72a47ac910
Katja Kranjec <Kranjec.katja1@gmail.com>
2020-11-10 22:16:15 +0100

    popravljen primer za stran brez filmov

--- zajemanje_podatkov.py
+++ zajemanje_podatkov.py
@@ -69,47 +69,48 @@ def izloci_deskriptorje(niz, deskriptorji):
     return ESRBdes
 
 def izloci_podatke(vsebina):
-    igra = vzorec_igre.search(vsebina).groupdict()
-    string = igra['datum'].replace(',', '')
-    date = datetime.strptime(string, '%b %d %Y').date()
-    igra['datum'] = date
-    igra['metascore'] = int(igra['metascore'])
-    igra['stevilo_glasov_metascore'] = int(igra['stevilo_glasov_metascore'])
-    igra['ocena_uporabnikov'] = float(igra['ocena_uporabnikov'])
-    igra['stevilo_glasov_uporabnikov'] = int(igra['stevilo_glasov_uporabnikov'])
-    oznaka = vzorec_ratinga.search(vsebina)
-    if oznaka:
-        igra['oznaka'] = oznaka['oznaka']
-    else:
-        igra['oznaka'] = None
-    developer = vzorec_developerja.search(vsebina)
-    if developer:
-        igra['developer'] = developer['developer']
-    else:
-        igra['developer'] = None
-    zanri = vzorec_zanrov.search(vsebina)
-    if zanri:
-        igra['zanri'] = zanri['zanri'].replace('  ', '').split(',')
-    else:
-        igra['zanri'] = []
-    stevilo_online_igralcev = vzorec_stevila_online_igralcev.search(vsebina)
-    if stevilo_online_igralcev:
-        igra['stevilo_online_igralcev'] = stevilo_online_igralcev['stevilo_online_igralcev']
-    else:
-        igra['stevilo_online_igralcev'] = None
-    also_on = vzorec_also_on.findall(vsebina)
-    also_on1 = str(also_on)
-    also_on2 = vzorec_also_on2.findall(also_on1)
-    igra['also_on'] = also_on2
-    ESRB = vzorec_ESRB_deskriptorjev.findall(vsebina)
-    igra['ESRB_deskriptorji'] = izloci_deskriptorje(str(ESRB), deskriptorji)
-    stevilo_igralcev = vzorec_stevila_igralcev.search(vsebina)
-    if stevilo_igralcev:
-        igra['stevilo_igralcev'] = stevilo_igralcev['stevilo_igralcev']
-    else:
-        igra['stevilo_igralcev'] = None
+    if vzorec_igre.search(vsebina):
+        igra = vzorec_igre.search(vsebina).groupdict()
+        string = igra['datum'].replace(',', '')
+        date = datetime.strptime(string, '%b %d %Y').date()
+        igra['datum'] = date
+        igra['metascore'] = int(igra['metascore'])
+        igra['stevilo_glasov_metascore'] = int(igra['stevilo_glasov_metascore'])
+        igra['ocena_uporabnikov'] = float(igra['ocena_uporabnikov'])
+        igra['stevilo_glasov_uporabnikov'] = int(igra['stevilo_glasov_uporabnikov'])
+        oznaka = vzorec_ratinga.search(vsebina)
+        if oznaka:
+            igra['oznaka'] = oznaka['oznaka']
+        else:
+            igra['oznaka'] = None
+        developer = vzorec_developerja.search(vsebina)
+        if developer:
+            igra['developer'] = developer['developer']
+        else:
+            igra['developer'] = None
+        zanri = vzorec_zanrov.search(vsebina)
+        if zanri:
+            igra['zanri'] = zanri['zanri'].replace('  ', '').split(',')
+        else:
+            igra['zanri'] = []
+        stevilo_online_igralcev = vzorec_stevila_online_igralcev.search(vsebina)
+        if stevilo_online_igralcev:
+            igra['stevilo_online_igralcev'] = stevilo_online_igralcev['stevilo_online_igralcev']
+        else:
+            igra['stevilo_online_igralcev'] = None
+        also_on = vzorec_also_on.findall(vsebina)
+        also_on1 = str(also_on)
+        also_on2 = vzorec_also_on2.findall(also_on1)
+        igra['also_on'] = also_on2
+        ESRB = vzorec_ESRB_deskriptorjev.findall(vsebina)
+        igra['ESRB_deskriptorji'] = izloci_deskriptorje(str(ESRB), deskriptorji)
+        stevilo_igralcev = vzorec_stevila_igralcev.search(vsebina)
+        if stevilo_igralcev:
+            igra['stevilo_igralcev'] = stevilo_igralcev['stevilo_igralcev']
+        else:
+            igra['stevilo_igralcev'] = None
 
-    return igra
+        return igra
 
 def izloci_gnezdene_podatke(igre):
     platforme, zanri, ESRB = [], [], []
@@ -133,7 +134,7 @@ igre_s_povezavami = []
 najdene_video_igre_s_povezavami = 0
 
 najdene_video_igre = 0
-
+seznam_slovarjev_iger = []
 
 
 for stran in range(STEVILO_STRANI):
@@ -166,7 +167,15 @@ with open(datoteka_s_slovarjem, 'r', encoding='utf-8') as f:
         datoteka = f'najbolj-znane-video-igre/video-igra{najdene_video_igre}.html'
         #orodja.shrani_spletno_stran(url, datoteka)
         vsebina = orodja.vsebina_datoteke(datoteka)
-
-        print(izloci_podatke(vsebina))
+        seznam_slovarjev_iger.append(izloci_podatke(vsebina))
+        #print(seznam_slovarjev_iger)
 
 print(najdene_video_igre)
+
+#platforme, zanri, ESRB = izloci_gnezdene_podatke(seznam_slovarjev_iger)
+#print(platforme)
+
+orodja.zapisi_csv(seznam_slovarjev_iger, ['naslov', 'publisher', 'datum', 'metascore', 'stevilo_glasov_metascore', 'ocena_uporabnikov', 'stevilo_glasov_uporabnikov', 'opis', 'oznaka', 'developer', 'stevilo_online_igralcev', 'stevilo_igralcev'], 'obdelani-podatki/videoigre.csv')
+#orodja.zapisi_csv(platforme, ['naslov', 'platforma'], 'obdelani-podatki/platforme.csv')
+#orodja.zapisi_csv(zanri, ['naslov', 'zanr'], 'obdelani-podatki/zanri.csv')
+#orodja.zapisi_csv(ESRB, ['naslov', 'ESRB_deskriptor'], 'obdelani-podatki/ESRB-deskriptorji.csv')
